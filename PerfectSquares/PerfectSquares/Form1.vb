93c6c1578615e18b8752e92244479e4547b62d4f
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:11:45 -0500

    solved 2 prob

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,17 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Dim n As Integer
+        n = 1
+        Do While (n <= 10)
+            lstResult.Items.Add("The perfect Square: " & n ^ 2)
+            n = n + 1
+
+        Loop
+
+
+        lstResult.Items.Add(n)
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,19 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim population As Integer
+        Dim year As Double
+        population = 7000
+        year = 2012
+
+        Do While (population > 6)
+            year = year - 50
+            population = CInt(population / 2)
+
+        Loop
+
+        MessageBox.Show("this year is " & year, "Error")
+
 
     End Sub
 End Class
